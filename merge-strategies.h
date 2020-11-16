1fa221108becf148a58458b24b3820be8a84491f
Alban Gruin <alban.gruin@gmail.com>
2020-11-16 11:21:50 +0100

    merge-index: libify merge_one_path() and merge_all()
    The "resolve" and "octopus" merge strategies do not call directly `git
    merge-one-file', they delegate the work to another git command, `git
    merge-index', that will loop over files in the index and call the
    specified command.  Unfortunately, these functions are not part of
    libgit.a, which means that once rewritten, the strategies would still
    have to invoke `merge-one-file' by spawning a new process first.
    
    To avoid this, this moves and renames merge_one_path(), merge_all(), and
    their helpers to merge-strategies.c.  They also take a callback to
    dictate what they should do for each file.  For now, to preserve the
    behaviour of `merge-index', only one callback, launching a new process,
    is defined.
    
    Signed-off-by: Alban Gruin <alban.gruin@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- merge-strategies.h
+++ merge-strategies.h
@@ -9,4 +9,21 @@ int merge_three_way(struct repository *r,
 		    const struct object_id *their_blob, const char *path,
 		    unsigned int orig_mode, unsigned int our_mode, unsigned int their_mode);
 
+typedef int (*merge_fn)(const struct object_id *orig_blob,
+			const struct object_id *our_blob,
+			const struct object_id *their_blob, const char *path,
+			unsigned int orig_mode, unsigned int our_mode, unsigned int their_mode,
+			void *data);
+
+int merge_one_file_spawn(const struct object_id *orig_blob,
+			 const struct object_id *our_blob,
+			 const struct object_id *their_blob, const char *path,
+			 unsigned int orig_mode, unsigned int our_mode, unsigned int their_mode,
+			 void *data);
+
+int merge_index_path(struct index_state *istate, int oneshot, int quiet,
+		     const char *path, merge_fn fn, void *data);
+int merge_all_index(struct index_state *istate, int oneshot, int quiet,
+		    merge_fn fn, void *data);
+
 #endif /* MERGE_STRATEGIES_H */
