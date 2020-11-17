f8d33ecbf3644a6339d87445eff0d0a729a8e6f4
Johannes Schindelin <johannes.schindelin@gmx.de>
2020-11-17 16:12:26 +0000

    t99*: adjust the references to the default branch name "main"
    Carefully excluding t9902, which sees independent development elsewhere
    at the time of writing, we use `main` as the default branch name in
    t9903. This trick was performed via
    
    	$ (cd t &&
    	   sed -i -e 's/master/main/g' -e 's/MASTER/MAIN/g' \
    		-e 's/Master/Main/g' -- t99*.sh lib-cvs.sh &&
    	   git checkout HEAD -- t9902\*)
    
    This allows us to define `GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME=main`
    for all tests (except the ones we specifically excluded for now).
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/test-lib.sh
+++ t/test-lib.sh
@@ -257,13 +257,13 @@ case "$TRASH_DIRECTORY" in
 esac
 
 case "$TEST_NUMBER" in
-3404|4013|5310|5526|6300|7064|7817)
+3404|4013|5310|5526|6300|7064|7817|9902)
 	# Avoid conflicts with patch series that are cooking at the same time
 	# as the patch series changing the default of `init.defaultBranch`.
 	GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME=master
 	export GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME
 	;;
-[0-8]*|9[0-8]*)
+*)
 	GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME=main
 	export GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME
 	;;
