9eefef5d32f0b0b7a8380b01a49a4aa6e10cdd2b
Johannes Schindelin <johannes.schindelin@gmx.de>
2020-11-17 16:12:22 +0000

    t8*: adjust the references to the default branch name "main"
    This trick was performed via
    
    	$ (cd t &&
    	   sed -i -e 's/master/main/g' -e 's/MASTER/MAIN/g' \
    		-e 's/Master/Main/g' -- t8*.sh annotate*.sh)
    
    This allows us to define `GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME=main`
    for those tests.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t8001-annotate.sh
+++ t/t8001-annotate.sh
@@ -7,7 +7,7 @@ PROG='git annotate'
 . "$TEST_DIRECTORY"/annotate-tests.sh
 
 test_expect_success 'annotate old revision' '
-	git annotate file master >actual &&
+	git annotate file main >actual &&
 	awk "{ print \$3; }" <actual >authors &&
 	test 2 = $(grep A <authors | wc -l) &&
 	test 2 = $(grep B <authors | wc -l)
